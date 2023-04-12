896ac3d6d9fb86097674bb0f37c215d5ae1ef3c9
Adithya-Lampros <adithyas.lamprostech@gmail.com>
2023-04-12 18:35:50 +0530

    Design Changes

--- src/components/Template.js
+++ src/components/Template.js
@@ -4,21 +4,23 @@ import "../styles/Template.scss";
 const Template = () => {
   return (
     <>
-      <div className="template-header">Template</div>
-      <div className="template-outer">
-        <div className="template-main">
-          <div>No</div>
-          <div>Title</div>
-          <div>Description</div>
-          <div>File</div>
-        </div>
-        <div className="template-data">
-          <div>1</div>
-          <div>Old Song Lyrics</div>
-          <div>
-            A old folke song about the wealth and prosperty of the new land
+      <div className="template-outer-main">
+        <div className="template-header">Template</div>
+        <div className="template-outer">
+          <div className="template-main">
+            <div>No</div>
+            <div>Title</div>
+            <div>Description</div>
+            <div>File</div>
           </div>
-          <div>Lyrics.pdf</div>
+          <div className="template-data">
+            <div>1</div>
+            <div>Old Song Lyrics</div>
+            <div>
+              A old folke song about the wealth and prosperty of the new land
+            </div>
+            <div>Lyrics.pdf</div>
+          </div>
         </div>
       </div>
     </>
